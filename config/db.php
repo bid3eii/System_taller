1779f54762b2374db33aeef2fbdb4847aa045ddb
BryanRivasDev <ing.bryanrivasq@gmail.com>
2026-03-10 14:36:57 -0600

    Fix Illegal mix of collations and add CSV encoding detection to import process

--- config/db.php
+++ config/db.php
@@ -25,7 +25,7 @@ if ($is_local) {
 }
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
